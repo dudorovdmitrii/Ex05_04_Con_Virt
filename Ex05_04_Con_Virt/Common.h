dc9a6e4b7cfd70a53bf516055167178263312206
dima <dudorovdmitrii@gmail.com>
2023-11-22 21:53:18 +0300

    version with 3 cards

--- Ex05_04_Con_Virt/Common.h
+++ Ex05_04_Con_Virt/Common.h
@@ -9,10 +9,10 @@ using namespace std;
 //макрос для определения кода нажатой клавиши
 #define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
 
-void task1(int initX, int initY, int initWidth, int initHeight, int dragValue);
+//void task1(int initX, int initY, int initWidth, int initHeight, int dragValue);
 //void task2(int initX, int initY, int initWidth, int initHeight, int dragValue);
-//void task3(int initX, int initY, int initWidth, int initHeight, int dragValue);
-void task4(int initX, int initY, int initWidth, int initHeight, int dragValue);
+void task3(int initX, int initY, int initWidth, int initHeight, int dragValue);
+//void task4(int initX, int initY, int initWidth, int initHeight, int dragValue);
 
 extern HPEN WhitePen;
 extern HPEN BlackPen;
